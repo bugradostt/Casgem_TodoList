8941f3869c7b42a8ff825e8bed00d6ec44cf7d0a
bugradostt <bugradostt@gmail.com>
2023-07-29 11:51:19 +0300

    first commit

---
+++ TodoList.PresentationLayer/Controllers/TodoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoList.PresentationLayer.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
